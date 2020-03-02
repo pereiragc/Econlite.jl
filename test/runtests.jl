a88fec013fe0906f07546d4c6fbcc10415adb014
Gustavo <gdp2116@columbia.edu>
2020-03-02 10:30:35 -0500

    Commit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using Econlite
 using Test
+using ForwardDiff
 
 @testset "Econlite.jl" begin
     # Write your own tests here.
+
+    tol = 1e-7
+    l = LogUtility()
+    crra = IsoElastic(0.42)
+    cara = CARA(1.2)
+
+    for ps in (l, crra, cara)
+        c = 1.23
+        u = util(c, ps)
+        cc = invutil(u, ps)
+
+        @test abs(c - cc) < tol
+
+        du=dutil(c, ps)
+        ccc=invdutil(du, ps)
+
+        @test abs(c - ccc) < tol
+
+
+        # check with forward diff
+        du_fd = ForwardDiff.derivative(t -> util(t, ps), c)
+
+        @test abs(du_fd - du) < tol
+    end
+
 end
